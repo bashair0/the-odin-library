1c22af7705f74231fef46db66a81d40d77c83eff
bashair0 <bisho5.ba@gmail.com>
2024-04-30 15:31:57 +0800

    Add functionality to the whole page

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
 # the-odin-library
+
+This is a solution to the the-odin project [Library project](https://www.theodinproject.com/lessons/node-path-javascript-library).
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Add books to the page with details of each book's: author, title and number of pages.
+- Remove a book from the shelf if the remove button was clicked.
+- Move a book to finished shelf if the finished button was clicked.
+
+### Screenshot
+
+![](assets/images/solution1.png)
+![](assets/images/solution2.png)
+![](assets/images/solution5.png)
+![](assets/images/solution3.png)
+![](assets/images/solution4.png)
+
+### Links
+
+- Solution URL: [solution URL](https://github.com/bashair0/)
+- Live Site URL: [live site URL](https://bashair0.github.io/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- java script
+
+### What I learned
+
+I learned how create so many objects that share the same characteristic using a constructor function.
+
+```js
+function Book(title, author, pagesNum, isRead) {
+  (this.title = title),
+    (this.author = author),
+    (this.pagesNum = pagesNum),
+    (this.isRead = isRead);
+}
+```
+
+## Author
+
+- github - [bashair0](https://github.com/bashair0)
+- Frontend Mentor - [@bashair0](https://www.frontendmentor.io/profile/bashair0)
+
+## Acknowledgments
+
+The design of the webpage was inspired by [Algirdas Jasaitis](https://dribbble.com/AlgirdasJasaitis)
--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const closeIcon = document.querySelector('.close-icon')
 const addButton = document.querySelector('[data-add-btn]')
 const formSection = document.querySelector('.form-section')
+const form = document.querySelector('.add-form')
 
-addButton.addEventListener('click', () => {
+function displayForm () {
   formSection.classList.remove('hidden')
-})
+}
 
-closeIcon.addEventListener('click', () => {
+function hideForm () {
   formSection.classList.add('hidden')
-})
+}
+
+addButton.addEventListener('click', displayForm)
+
+closeIcon.addEventListener('click', hideForm)
 
 const myLibrary = []
 const finished = []
@@ -16,80 +21,128 @@ const finished = []
 const submitButton = document.querySelector('[data-submit]')
 submitButton.addEventListener('click', e => {
   e.preventDefault()
-
-  const isFinished = document.querySelector('[data-finished]')
-  const titleValue = document.querySelector('[data-title]').value
-  const authorValue = document.querySelector('[data-author]').value
-  const pagesNumValue = document.querySelector('[data-pages-num]').value
-  const newBook = new Book(titleValue, authorValue, pagesNumValue)
-
-  if (isFinished.checked) {
-    finished.push(newBook)
-  } else {
-    myLibrary.push(newBook)
-  }
-
-  console.log(newBook.info())
-  const form = document.querySelector('.add-form')
-  form.reset()
-  formSection.classList.add('hidden')
   addBookToLibrary()
+
+  form.reset()
+  hideForm()
+
+  updateLibrary()
 })
 
 function Book (title, author, pagesNum, isRead) {
   ;(this.title = title),
     (this.author = author),
     (this.pagesNum = pagesNum),
-    (this.isRead = isRead ? isRead : 'not read yet'),
+    (this.isRead = isRead),
     (this.info = function () {
       return `${this.title} by ${this.author}, ${this.pagesNum} pages, ${this.isRead}`
     })
 }
 
 function addBookToLibrary () {
+  const isFinished = document.querySelector('[data-finished]')
+  const titleValue = document.querySelector('[data-title]').value
+  const authorValue = document.querySelector('[data-author]').value
+  const pagesNumValue = document.querySelector('[data-pages-num]').value
+  const newBook = new Book(
+    titleValue,
+    authorValue,
+    pagesNumValue,
+    isFinished.checked
+  )
+
+  if (isFinished.checked) {
+    finished.push(newBook)
+  } else {
+    myLibrary.push(newBook)
+  }
+}
+
+function removeBook (library, index) {
+  if (library === 'myLib') {
+    myLibrary.splice(index, 1)
+  } else {
+    finished.splice(index, 1)
+  }
+  updateLibrary()
+}
+
+function moveToFinished (index) {
+  let finishedBook = myLibrary.splice(index, 1)
+  let newFinishedBook = new Book(
+    finishedBook[0].title,
+    finishedBook[0].author,
+    finishedBook[0].pagesNum
+  )
+  finished.push(newFinishedBook)
+  updateLibrary()
+}
+
+function updateLibrary () {
   const bookList = document.querySelector('[data-book-list]')
   bookList.innerHTML = ''
   const finishedList = document.querySelector('[data-finished-list]')
   finishedList.innerHTML = ''
-  for (let book in myLibrary) {
-    const bookCover = document.createElement('li')
-    bookCover.classList.add('book-cover')
+  for (let i = 0; i < myLibrary.length; i++) {
+    const cover = document.createElement('div')
+    cover.classList.add('cover')
+    cover.innerHTML = `
+    <li class="book-cover">
+                  <span class="book-author">${myLibrary[i].author}</span>
+                  <span class="book-title">${myLibrary[i].title}</span>
+                  <span class="book-pages">${myLibrary[i].pagesNum}</span>
+                </li>
+                <div class="cover-buttons">
+                  <button type="button" class="btn cover-btn" onclick="moveToFinished(${i})">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      height="20px">
+                      <path
+                        d="M9,20.42L2.79,14.21L5.62,11.38L9,14.77L18.88,4.88L21.71,7.71L9,20.42Z" />
+                    </svg>
+                    Finished
+                  </button>
+                  <button type="button" class="btn cover-btn" onclick="removeBook('myLib', ${i})">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      height="20px"
+                      fill="#d5304b">
+                      <path
+                        d="M19,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3M19,19H5V5H19V19M17,8.4L13.4,12L17,15.6L15.6,17L12,13.4L8.4,17L7,15.6L10.6,12L7,8.4L8.4,7L12,10.6L15.6,7L17,8.4Z" />
+                    </svg>
+                    Remove
+                  </button>
+                </div>`
 
-    const bookCoverAuthor = document.createElement('span')
-    bookCoverAuthor.classList.add('book-author')
-    bookCoverAuthor.textContent = myLibrary[book].author
-    bookCover.appendChild(bookCoverAuthor)
-
-    const bookCoverTitle = document.createElement('span')
-    bookCoverTitle.classList.add('book-title')
-    bookCoverTitle.textContent = myLibrary[book].title
-    bookCover.appendChild(bookCoverTitle)
-
-    const BookPages = document.createElement('span')
-    BookPages.classList.add('book-pages')
-    BookPages.textContent = myLibrary[book].pagesNum
-    bookCover.appendChild(BookPages)
-    bookList.appendChild(bookCover)
+    bookList.appendChild(cover)
   }
 
-  for (shu in finished) {
-    const bookCover = document.createElement('li')
-    bookCover.classList.add('book-cover')
+  for (let i = 0; i < finished.length; i++) {
+    const cover = document.createElement('div')
+    cover.classList.add('cover')
+    cover.innerHTML = `
+    <li class="book-cover">
+                  <span class="book-author">${finished[i].author}</span>
+                  <span class="book-title">${finished[i].title}</span>
+                  <span class="book-pages">${finished[i].pagesNum}</span>
+                </li>
+                <div class="cover-buttons">
+                  
+                  <button type="button" class="btn cover-btn" onclick="removeBook(finished,${i})">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      height="20px"
+                      fill="#d5304b">
+                      <path
+                        d="M19,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3M19,19H5V5H19V19M17,8.4L13.4,12L17,15.6L15.6,17L12,13.4L8.4,17L7,15.6L10.6,12L7,8.4L8.4,7L12,10.6L15.6,7L17,8.4Z" />
+                    </svg>
+                    Remove
+                  </button>
+                </div>`
 
-    const bookCoverAuthor = document.createElement('span')
-    bookCoverAuthor.classList.add('book-author')
-    bookCoverAuthor.textContent = finished[shu].author
-    bookCover.appendChild(bookCoverAuthor)
-
-    const bookCoverTitle = document.createElement('span')
-    bookCoverTitle.classList.add('book-title')
-    bookCoverTitle.textContent = finished[shu].title
-    bookCover.appendChild(bookCoverTitle)
-
-    const BookPages = document.createElement('span')
-    BookPages.classList.add('book-pages')
-    BookPages.textContent = finished[shu].pagesNum
-    bookCover.appendChild(BookPages)
-    finishedList.appendChild(bookCover)
+    finishedList.appendChild(cover)
   }
 }
